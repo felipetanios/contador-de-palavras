7797211de0c26b74f8f6afee191f6eac75b63272
Felipe Tanios <felipe.tanios@gmail.com>
2017-08-08 10:41:03 -0300

    arquivo finalizado com todos os testes funcionando

--- src/main.c
+++ src/main.c
@@ -9,14 +9,45 @@
 
 int main() {
 
-  char c;
-
-  c = 1;
+  char c = 1;
+  int counter = 0;
+  int ponto;
+  int flag;
 
   while (c != '\n') {
+    ponto = 1;
+    flag = 1;
     scanf("%c", &c);
+  
+    //se esta numa palavra
+    if ((c >= 'A' && c <='Z') || (c >= 'a' && c <= 'z')){
+      counter++;
+      //vai ateh o final dela
+      while ((c >= 'A' && c <='Z') || (c >= 'a' && c <= 'z') || (c>= '0' && c <='9')){
+        scanf("%c", &c);
+      }
+    }
+
+    //se esta num numero
+    else if (c>= '0' && c <='9'){
+      counter++;
+      //vai ateh o final dele
+      while((c>= '0' && c <='9') || ((c =='.'||c ==',') && flag)) {
+        scanf("%c", &c);
+        //se eh o primeiro ponto no numero
+        if ((c =='.'||c ==',') && ponto){
+          ponto = 0;
+        }
+        //se eh o segundo ponto no numero
+        else if ((c =='.'||c ==',') && !ponto){
+          flag = 0;
+        }
+      }
+    }
+
+    //acabou uma palavra
   }
 
-  printf("1\n");
+  printf("%d\n", counter);
   return 0;
 }
